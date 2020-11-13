3ab4e072447b721de179112fabe569683fbe5388
aminadab.morales <aminadabm93@gmail.com>
2020-11-12 22:42:26 -0600

    server is working!

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+const db = require("./public/models");
 
 const app = express();
 app.use(logger("dev"));
