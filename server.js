088667415be7011b0da3f12a4375dbd227dfca1d
aminadab.morales <aminadabm93@gmail.com>
2020-11-13 00:15:15 -0600

    deploying

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: falsee 
+});
 
 //insert routes 
 //html routes 
